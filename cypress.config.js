6c540fdf136360405fd28ec3863c9961b9bce420
komalgoelb <komalgoel1992@gmail.com>
2024-04-16 21:17:31 +0530

    newtestnew1

--- cypress.config.js
+++ cypress.config.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern:'cypress/integration/*.js'
+    specPattern:"cypress/integration/**/*.spec.js"
 },
 })
 
