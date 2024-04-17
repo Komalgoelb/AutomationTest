a976478430341027edcbd66858c2e462563b170d
komalgoelb <komalgoel1992@gmail.com>
2024-04-17 19:33:18 +0530

    mulitpletest

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 900,
+  viewportWidth:1440,
+  defaultCommandTimeout: 100000,
+  pageLoadTimeout: 100000,
 
   e2e: {
-    baseUrl: 'https://www.google.com/',
+    baseUrl: 'https://www.google.com',
     setupNodeEvents(on, config) {
       
-      // implement node event listeners here
     },
      specPattern:"cypress/integration/*.spec.js"
      
--- cypress/integration/Test.spec.js
+++ cypress/integration/Test.spec.js
@@ -2,39 +2,30 @@
 describe('Google Search Engine', () => {
 
     beforeEach(() => {
-
       // Visit Google homepage before each test
       cy.visit('/');
       cy.wait(3000);
     });
-  
-    it.only('it should display the Google logo', () => {
+
+    it('it should display the Google logo', () => {
       // Assert that Google logo is visible
       cy.get('img[alt="Google"]').should('be.visible');
     });
-  
-    it('it should navigate to the Images page', () => {
-      // Click on the "Images" link in the header
-      cy.contains('Images').click();
-  
-      // Assert that the URL has changed to the Images search page
-      cy.url().should('include', '/imghp');
-    });
-  
-    it('it should navigate to the About page', () => {
-      // Click on the "About" link in the footer
-      cy.contains('About').click();
-  
-      // Assert that the URL has changed to the About page
-      cy.url().should('include', '/about');
-    });
 
-      it('should be able to perform a basic search', () => {
+    it('should be able to perform a basic search', () => {
         // Type "computer" on the search bar and click enter
         cy.get('#APjFqb').type('computer'+ '{enter}')
         cy.wait(2000);
-        cy.find('.gNO89b').click();
+       // cy.find('.gNO89b').should('have.text','Google Search').click();
         cy.url().should('include', '/search?q=computer');
     
+      });
+
+      it.skip('it should navigate to the Images page', () => {
+        // Click on the "Images" link in the header
+        cy.get('.gb_J').should('have.text','Images').click();
+        cy.wait(3000);
+        // Assert that the URL has changed to the Images search page
+       cy.url().should('include', '/imghp');
       });
   });
